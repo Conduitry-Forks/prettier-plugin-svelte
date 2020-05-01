b96958eafae61040f7053de05f578a9540edf6a5
Conduitry <git@chor.date>
2020-05-01 11:01:15 -0400

    version 0.7.1 (release)

--- src/lib/getText.ts
+++
@@ -1,6 +0,0 @@
-import { ParserOptions } from 'prettier';
-import { Node } from '../print/nodes';
-
-export function getText(node: Node, options: ParserOptions) {
-    return options.originalText.slice(options.locStart(node), options.locEnd(node));
-}
